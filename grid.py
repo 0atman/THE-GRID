3ecd492dc43a964b2accb29a7f54e7e0544b1f85
Tristram Oaten <tristram@oaten.name>
2016-03-06 15:42:01 +0000

    files support and help text

--- grid.py
+++ grid.py
@@ -2,7 +2,6 @@
 import requests
 from getpass import getpass
 import json
-from pprint import pprint
 
 from auth import api_key
 
@@ -52,59 +51,102 @@ class Player(object):
         self.health = 100
         self.x = 0
         self.y = 0
+        self.file_store = []
 
-    def quit(self):
+    def quit(self, *args):
         print("\n%s jacks out of THE GRID. Goodbye." % self.name)
         remove_from_room(self)
         self.health = 0
 
-    def help(self):
-        print("Commands:", ", ".join(Commands.keys()))
-
-    def status(self):
+    def status(self, *args):
+        "Show current GRID position."
         room_raw = get(
             'GRID:%s,%s' % (str(p.x), str(p.y))
         )
         room_data = json.loads(room_raw)
         room_data['players'].remove(p.name)
-        print("GRID(%d,%d)" % (p.x, p.y))
         if room_data['players']:
             print("HERE: " + ", ".join(room_data['players']))
 
-    def north(self):
+    def north(self, *args):
         remove_from_room(p)
         self.y = self.y + 1
         add_to_room(p)
         self.status()
 
-    def south(self):
+    def south(self, *args):
         remove_from_room(p)
         self.y = self.y - 1
         add_to_room(p)
         self.status()
 
-    def east(self):
+    def east(self, *args):
         remove_from_room(p)
         self.x = self.x + 1
         add_to_room(p)
         self.status()
 
-    def west(self):
+    def west(self, *args):
         remove_from_room(p)
         self.x = self.x - 1
         add_to_room(p)
         self.status()
 
+    def writefile(self):
+        print("Write your file below, to finish put a period '.' on a line by itself")
+        input_list = []
+
+        while True:
+            input_str = input('" ')
+            if input_str == ".":
+                break
+            else:
+                input_list.append(input_str)
+        self.file_store.append("\n".join(input_list))
+        self.save()
+
+    def files(self, *args):
+        if args:
+            if args[0].lower() in ['new', 'create', 'write', 'add']:
+                self.writefile()
+                return
+            if args[0].lower() in ['delete', 'del', 'rm']:
+                try:
+                    del self.file_store[int(args[1])]
+                    self.save()
+                except ValueError:
+                    print("Bad file ID.")
+                except IndexError:
+                    print("Can't find that file ID")
+                return
+        if not self.__dict__.get('file_store'):
+            self.file_store = []
+            self.save()
+        for n, file in enumerate(self.file_store):
+            print('')
+            print("file #" + str(n))
+            print(file)
+
+    def save(self):
+        put('PLAYER:%s' % self.name, json.dumps(self.__dict__))
+
     def initialise(self, new=False):
         if new:
-            put('PLAYER:%s' % p.name, json.dumps(p.__dict__))
-        p.__dict__ = json.loads(get('PLAYER:%s' % p.name))
+            self.save()
+        self.__dict__ = json.loads(get('PLAYER:%s' % p.name))
 
-        if p.name in get('GRID:%s,%s' % (str(p.x), str(p.y))):
+        if self.name in get('GRID:%s,%s' % (str(p.x), str(p.y))):
             print("Already logged in. If this is in error, please contact oatman@bgr0.com.")
             raise
 
         add_to_room(self)
+
+
+def help(*args):
+    if args:
+        print(Commands.get(args[0]).__doc__)
+    else:
+        print("Commands:", ", ".join(Commands.keys()))
 
 
 def get_room(x, y):
@@ -122,19 +164,28 @@ def scan():
         ])
 
 
+def position(player):
+    room_raw = get(
+        'GRID:%s,%s' % (str(p.x), str(p.y))
+    )
+    room_data = json.loads(room_raw)
+    room_data['players'].remove(p.name)
+    return "GRID(%d,%d)" % (p.x, p.y)
+
 if __name__ == '__main__':
 
     p = Player()
 
     Commands = {
       'quit': p.quit,
-      'help': p.help,
+      'help': help,
       'status': p.status,
       'north': p.north,
       'south': p.south,
       'east': p.east,
       'west': p.west,
-      'scan': scan
+      'scan': scan,
+      'files': p.files,
       }
     try:
         while(True):
@@ -162,19 +213,22 @@ if __name__ == '__main__':
         p.status()
 
         while(p.health > 0):
-            line = input("> ")
+            line = input(position(p) + "> ")
             args = line.split()
-            if len(args) > 0:
-                commandFound = False
-            for c in Commands.keys():
-                if args[0] == c[:len(args[0])]:
-                    Commands[c]()
-                    commandFound = True
-                    break
-            if not commandFound:
+
+            if Commands.get(args[0]):
+                Commands[args[0]](*args[1:])
+            else:
                 if args[0] == 'debug' and p.name == 'oatman':
                     import ipdb; ipdb.set_trace()
                 print("SYNTAX ERROR (type 'help' for commands)")
+    except KeyboardInterrupt:
+        pass
+    except EOFError:
+        pass
     except Exception as e:
-        print(e)
+        import socket
+        if not socket.gethostname() == 'bgr0.com':
+            raise e
+    finally:
         p.quit()
